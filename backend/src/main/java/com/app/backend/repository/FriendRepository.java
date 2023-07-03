3050f3b3a809dd1f41a93070f33692294ea4f549
iamclaudiar <59837410+iamclaudiar@users.noreply.github.com>
2023-07-03 23:17:55 +0300

    Delete backend directory
    Updated code will be added.

--- backend/src/main/java/com/app/backend/repository/FriendRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.backend.repository;
-
-import com.app.backend.model.Friend;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FriendRepository extends CrudRepository<Friend, Integer> {
-}
